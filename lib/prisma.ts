afcfcd0ff89aa143120f361357c69494f230cc1d
AtharvShelke <shelkeatharv964@gmail.com>
2026-03-10 16:57:35 +0530

    feat: Add Prisma client setup with PostgreSQL adapter, connection pooling, and global singleton instance.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,25 +3,27 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 import { Pool } from "pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
 const prismaClientSingleton = () => {
-    const pool = new Pool({
-        connectionString,
-        max: 20,
-        idleTimeoutMillis: 30000,
-        connectionTimeoutMillis: 5000,
-    });
-    const adapter = new PrismaPg(pool);
-    return new PrismaClient({ adapter });
+  const pool = new Pool({
+    connectionString,
+    ssl: { rejectUnauthorized: false },
+    max: 1,
+  });
+
+  const adapter = new PrismaPg(pool);
+
+  return new PrismaClient({ adapter });
 };
 
 declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+export const prisma =
+  globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export { prisma };
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
